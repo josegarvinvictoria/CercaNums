b056e6adad2e0a530a7b453ebb73627a54289698
jose <josegarvinvictoria@gmail.com>
2014-10-23 22:16:55 +0200

    Comentari editat

--- CercaNums/src/cercanum/CercaNum.java
+++ CercaNums/src/cercanum/CercaNum.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class CercaNum {
-//Hola que tal
+//Hola que talll
     static int[] numeros = new int[500000];
     static boolean numTrobat = false;
 
